8ecfcb1a41b06735810cf54e1455fd0b68978124
Alienchelle <rachelle.ballew@gmail.com>
2026-04-25 16:38:44 -0700

    Create portfolio website

---
+++ script.js
@@ -0,0 +1,4 @@
+const footerText = document.getElementById("footer-text");
+const currentYear = new Date().getFullYear();
+
+footerText.textContent = `© ${currentYear} Rachelle. Built with HTML, CSS, and JavaScript.`;
